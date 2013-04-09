1d5816c82a4ae87172b435e9827a8f357d6b4415
Burak Sezer <bburaksezer@gmail.com>
2013-04-09 23:03:16 +0300

    changes about user management and feed sync(first sync hack)

--- urls.py
+++ urls.py
@@ -6,10 +6,10 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'apps.frontend.views.home', name='home'),
+    url(r'^accounts/', include('userena.urls')),
     url(r'^auth/', 'apps.frontend.views.auth', name='auth'),
     url(r'^explorer/(?P<feed_id>[^\.]+)/(?P<slug>[^\.]+)', 'apps.frontend.views.explorer', name='explorer'),
     url(r'^subscribe/', 'apps.frontend.views.subscribe', name='subscribe'),
-
     # Examples:
     # url(r'^$', 'FeedCraft.views.home', name='home'),
     # url(r'^FeedCraft/', include('FeedCraft.foo.urls')),
